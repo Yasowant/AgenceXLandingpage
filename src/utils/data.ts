f7bd03c3c80502dd9b11f2f4dfe4f1e6d508cad3
Yasowant <yasowant1998@gmail.com>
2025-01-26 14:05:16 +0530

    first commit

---
+++ src/utils/data.ts
@@ -0,0 +1,40 @@
+const services = [
+  {
+    title: 'Social Media Marketing',
+    description:
+      'Leverage social media platforms to increase brand awareness, engage audiences, and drive traffic to your business through creative campaigns.',
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" /></svg>`,
+  },
+  {
+    title: 'Amazon Affiliate Marketing',
+    description:
+      "Earn commissions by promoting Amazon products using affiliate links and strategies tailored to your audience's interests.",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" /></svg>`,
+  },
+  {
+    title: 'Email Marketing',
+    description:
+      'Craft personalized email campaigns to nurture leads, drive conversions, and build lasting relationships with your customers.',
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" /></svg>`,
+  },
+  {
+    title: 'Content Marketing',
+    description:
+      'Develop and distribute valuable, relevant, and consistent content to attract and retain your target audience.',
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" /></svg>`,
+  },
+  {
+    title: 'Influencer Marketing',
+    description:
+      'Partner with influencers to promote your products or services to their engaged and loyal audiences.',
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" /></svg>`,
+  },
+  {
+    title: 'SEO Optimization',
+    description:
+      'Optimize your website and content for search engines to increase organic traffic and improve rankings on search result pages.',
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" /></svg>`,
+  },
+];
+
+export { services };
